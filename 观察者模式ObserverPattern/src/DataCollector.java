82dcaa5b3e1208a541e686ba09cbe8ae5106d2a9
YYkwSir <877891173@qq.com>
2019-04-01 10:23:24 +0800

    fix

--- 观察者模式ObserverPattern/src/DataCollector.java
+++
@@ -1,5 +0,0 @@
-public interface DataCollector {
-    public void registerObserver(Observer o);
-    public void removeObserver(Observer o);
-    public void notifyAllObservers();
-}
--- 观察者模式ObserverPattern/src/Displayer.java
+++
@@ -1,3 +0,0 @@
-public interface Displayer {
-    public void display();
-}
